481842df313a4cd1c0848d5a8b77f7d97b6303ce
MikhailPimenov <misha4git@yandex.ru>
2020-07-28 05:38:21 +0300

    prototype version 2 with smart pointers, all in one file

--- main.cpp
+++ main.cpp
@@ -12,39 +12,45 @@ enum Warrior_Id
     Horseman_Id,
 };
 class Warrior;
-using Prototypes = std::map < Warrior_Id, Warrior * >;
+using Prototypes = std::map < Warrior_Id, std::unique_ptr < Warrior > >;
 Prototypes& getPrototypes()
 {
     static Prototypes instance;
     return instance;
 }
 
-
-
-
 class Warrior
 {
 public:
     virtual ~Warrior() {}
 
-    virtual Warrior* clone() const = 0;
+    virtual std::unique_ptr < Warrior > clone() const = 0;
     virtual void info() const = 0;
 
-    static Warrior* createWarrior ( Warrior_Id id )
+    static std::unique_ptr < Warrior > createWarrior ( Warrior_Id id )
     {
         Prototypes &prototypes = getPrototypes();
 
         if ( prototypes.find ( id ) != prototypes.cend() )
-            return prototypes.at ( id )->clone();
+            return prototypes.at ( id ).get()->clone();
 
         return nullptr;
     }
 
-    static void addPrototype ( Warrior_Id id, Warrior *prototype )
+    static void addPrototype ( Warrior_Id id, std::unique_ptr < Warrior > prototype )
     {
         Prototypes &prototypes = getPrototypes();
-        prototypes.insert ( std::pair < Warrior_Id, Warrior * > ( id, prototype ) );
+        prototypes.insert (
+                    std::pair < Warrior_Id,
+                                std::unique_ptr < Warrior > > ( id, std::move ( prototype ) ) );
     }
+
+    static void deletePrototype ( Warrior_Id id )
+    {
+        Prototypes &prototypes = getPrototypes();
+        prototypes.erase ( prototypes.find ( id ) );
+    }
+
 };
 
 class OnlyToAddPrototype
@@ -56,7 +62,8 @@ public:
     Infantryman() {}
     Infantryman ( OnlyToAddPrototype )
     {
-        Warrior::addPrototype ( Infantryman_Id, this );
+        Warrior::addPrototype ( Warrior_Id::Infantryman_Id,
+                                std::make_unique < Infantryman > ( *this ) );
     }
 
     virtual void info() const override
@@ -64,30 +71,89 @@ public:
         std::cout << "Infantryman\n";
     }
 
-    virtual Warrior* clone() const override
+    virtual std::unique_ptr < Warrior > clone() const override
     {
-        return new Infantryman ( *this );
+        return std::make_unique < Infantryman > ( *this );
     }
 
 private:
     static Infantryman s_prototype;
 };
 
+class Horseman : public Warrior
+{
+public:
+    Horseman() {}
+    Horseman ( OnlyToAddPrototype )
+    {
+        Warrior::addPrototype ( Warrior_Id::Horseman_Id,
+                                std::make_unique < Horseman > ( *this ) );
+    }
+
+    virtual void info() const override
+    {
+        std::cout << "Horseman\n";
+    }
+
+    virtual std::unique_ptr < Warrior > clone() const override
+    {
+        return std::make_unique < Horseman > ( *this );
+    }
+
+private:
+    static Horseman s_prototype;
+};
+
+class Archer : public Warrior
+{
+public:
+    Archer() {}
+    Archer ( OnlyToAddPrototype )
+    {
+        Warrior::addPrototype ( Warrior_Id::Archer_Id,
+                                std::make_unique < Archer > ( *this ) );
+    }
+
+    virtual void info() const override
+    {
+        std::cout << "Archer\n";
+    }
+
+    virtual std::unique_ptr < Warrior > clone() const override
+    {
+        return std::make_unique < Archer > ( *this );
+    }
+
+private:
+    static Archer s_prototype;
+};
+
+
 Infantryman Infantryman::s_prototype = Infantryman ( OnlyToAddPrototype() );
+Horseman       Horseman::s_prototype = Horseman ( OnlyToAddPrototype() );
+Archer           Archer::s_prototype = Archer ( OnlyToAddPrototype() );
 
 void testPrototype()
 {
-    std::vector < Warrior * > vector;
-    vector.push_back ( Warrior::createWarrior ( Infantryman_Id ) );
-    vector.push_back ( Warrior::createWarrior ( Infantryman_Id ) );
-    vector.push_back ( Warrior::createWarrior ( Infantryman_Id ) );
-    vector.push_back ( Warrior::createWarrior ( Infantryman_Id ) );
+    std::vector < std::unique_ptr < Warrior > > vector;
+    vector.push_back ( Warrior::createWarrior ( Warrior_Id::Horseman_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Warrior_Id::Infantryman_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Warrior_Id::Archer_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Warrior_Id::Archer_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Warrior_Id::Archer_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Warrior_Id::Archer_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Warrior_Id::Infantryman_Id ) );
 
     for ( const auto &element : vector )
-        element->info();
+        if ( element )
+            element->info();
+
+    std::cout << "testPrototype(): end\n";
 }
 int main()
 {
     testPrototype();
+
+    std::cout << "main(): end\n";
     return 0;
 }
