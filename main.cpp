037f8c420d9dca81da38db09c5d28849af9d5c86
MikhailPimenov <misha4git@yandex.ru>
2020-07-28 04:49:13 +0300

    prototype version 1 with no smart pointers and memory losing

--- main.cpp
+++ main.cpp
@@ -1,26 +1,93 @@
 #include <iostream>
+#include <map>
 #include <memory>
+#include <vector>
 
-#include "Contact/contact.h"
-#include "ContactBuilder/contactbuilder.h"
+// patterns: prototype
 
-// patterns: builder part3
-
-void testBuilder()
+enum Warrior_Id
 {
-    ContactBuilder contactBuilder;
-    std::unique_ptr < Contact > contact = contactBuilder.age ( 20 )
-                                                        .mail ( "asdf@mail.ru" )
-                                                        .name ( "Andrew" )
-                                                        .surname ( "Prokofiev" )
-                                                        .phone ( "123456789" )
-                                                        .address ( "Super street 14" )
-                                                        .build();
+    Infantryman_Id,
+    Archer_Id,
+    Horseman_Id,
+};
+class Warrior;
+using Prototypes = std::map < Warrior_Id, Warrior * >;
+Prototypes& getPrototypes()
+{
+    static Prototypes instance;
+    return instance;
+}
 
-    contact.get()->print();
+
+
+
+class Warrior
+{
+public:
+    virtual ~Warrior() {}
+
+    virtual Warrior* clone() const = 0;
+    virtual void info() const = 0;
+
+    static Warrior* createWarrior ( Warrior_Id id )
+    {
+        Prototypes &prototypes = getPrototypes();
+
+        if ( prototypes.find ( id ) != prototypes.cend() )
+            return prototypes.at ( id )->clone();
+
+        return nullptr;
+    }
+
+    static void addPrototype ( Warrior_Id id, Warrior *prototype )
+    {
+        Prototypes &prototypes = getPrototypes();
+        prototypes.insert ( std::pair < Warrior_Id, Warrior * > ( id, prototype ) );
+    }
+};
+
+class OnlyToAddPrototype
+{};
+
+class Infantryman : public Warrior
+{
+public:
+    Infantryman() {}
+    Infantryman ( OnlyToAddPrototype )
+    {
+        Warrior::addPrototype ( Infantryman_Id, this );
+    }
+
+    virtual void info() const override
+    {
+        std::cout << "Infantryman\n";
+    }
+
+    virtual Warrior* clone() const override
+    {
+        return new Infantryman ( *this );
+    }
+
+private:
+    static Infantryman s_prototype;
+};
+
+Infantryman Infantryman::s_prototype = Infantryman ( OnlyToAddPrototype() );
+
+void testPrototype()
+{
+    std::vector < Warrior * > vector;
+    vector.push_back ( Warrior::createWarrior ( Infantryman_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Infantryman_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Infantryman_Id ) );
+    vector.push_back ( Warrior::createWarrior ( Infantryman_Id ) );
+
+    for ( const auto &element : vector )
+        element->info();
 }
 int main()
 {
-    testBuilder();
+    testPrototype();
     return 0;
 }
