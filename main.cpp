6ec46edbdce72d57af0a6ca80fc0946b44789e1e
MikhailPimenov <misha4git@yandex.ru>
2020-10-30 16:13:40 +0300

    Revert "patterns : creational - prototype2"
    This reverts commit 4b1aeda77ec31c2830785579d10b7950b4383cc8.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-// patterns: prototype2
+// patterns: visitor
 
-#include "Creational/prototype/prototype2/test_prototype2.h"
+#include "Behavioral/Visitor/Visitor1/test_visitor1.h"
 
 int main()
 {
     std::cout << "main(): start\n";
 
-    Prototype2_::test_prototype2();
+    Visitor1_::test_visitor1();
 
     std::cout << "main(): end\n";
 
